2ddca4d8e85be449ccfe2f88db64182d2528925a
Douwe <douwe.ravers@hotmail.com>
2021-04-25 02:58:04 +0200

    Started on the story

--- Scenes/StartMenu.gd
+++ Scenes/StartMenu.gd
@@ -1,4 +1,8 @@
 extends Control
 
 func _on_Button_pressed():
-	Singleton.load_scene("Game")
+	Singleton.load_scene("StoryBasic")
+
+
+func _on_HowToPlay_pressed():
+	pass # Replace with function body.
